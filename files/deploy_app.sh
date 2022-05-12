a762a428862965063ffd424352304ebbbf471f13
rishi160 <62858492+rishi160@users.noreply.github.com>
2022-05-12 18:17:12 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Big Rishi in da house. 
+  Welcome to ${PREFIX}'s app. Big Rishi in da house PT2 . 
   <!-- END -->
   
   </div>
